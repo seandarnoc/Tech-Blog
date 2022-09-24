347010d4df10db5f1de7238c76231a94c9715ed6
Sean Conrad <sean.w.conrad@gmail.com>
2022-09-24 11:23:27 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const session = require('express-session');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const app = express();
+const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { 
+        maxAge: 20000,
+        sameSite: true },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on localhost port: ${PORT}`));
+})
